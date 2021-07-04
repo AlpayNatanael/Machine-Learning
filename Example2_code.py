74f97b43f65b8711a126b4bc57a443cfd88452de
Natanael <alpay100@mail.chapman.edu>
2021-07-03 18:53:43 -0700

    add

--- Example2_code.py
+++ Example2_code.py
@@ -11,11 +11,11 @@ def ActivFunc(v):
     else:
         x = 1
     return x
-    
-    
+
+
 # loading the data
 # %matplotlib inline
-def load_data_set(show = True):
+def load_data_set(show = True, line = False, weights = []):
     """ Show- true graph, else no grapth"""
     #https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
 
@@ -36,70 +36,100 @@ def load_data_set(show = True):
         plt.ylabel('petal length')
         plt.legend(loc='upper right')
         plt.show()
+
+    if (line == True):
+        plt.scatter(X[:50, 0], X[:50, 1],
+                    color='green', marker='x', label='setosa')
+
+        # last thifty entires
+        plt.scatter(X[50:100, 0], X[50:100, 1],
+                    color='red', marker='o', label='versicolor')
+        plt.xlabel('sepal length')
+        plt.ylabel('petal length')
+        plt.legend(loc='upper right')
+
+        line_plt = []
+        # Here i am calculating slope and intercept with given three weights
+        for i in np.linspace(np.amin(X[:50,:1]),np.amax(X[:50,:1])):
+            slope = -(weights[0]/weights[2])/(weights[0]/weights[1])
+            intercept = -weights[0]/weights[2]
+
+            #y =mx+c, m is slope and c is intercept
+            y = (slope*i) + intercept
+            plt.plot(i, y,'ko')
+            #line_plt.append([i,y,'ko'])
+
+        #plt.scatter(line_plt[:,0], line_plt[:,1],
+        #            color='red', marker='o', label='versicolor')
+        #plt.show()
+
+
     else:
+        #X = X[:50,]
+        #y = X[50:100,]
         return X,y
-    
 
 
 
-    
+
+
 class Perceptron(object):
     """
     self - the thing.
     learning_rate - the wieght, eta.
     random_state - to see later.
     """
-    
+
     # constractor
     def __init__(self, learning_rate=0.01, n_iter=100, random_state=1):
         self.learning_rate = learning_rate
         self.n_iter = n_iter
         self.random_state = random_state
-        
-        
+
+
     # maps the traings data X and the target Y
     def fit(self, X, y):
         rand = np.random.RandomState(self.random_state)
-        
+
         # create a vector of weights of of size 'size of x + 1' with first entery as bias.
         self.weights = rand.normal(loc=0.0, scale=0.01, size=1 +  X.shape[1])
-        
+
         self.errors_ = []
         for _ in range(self.n_iter):
             errors = 0
             for x, target in zip(X, y):
                 # the update delta= eta(d-x)
                 update = self.learning_rate * (target - self.predict(x))
-                
+
                 # We update the weight
                 self.weights[1:] += update * x
-                
+
                 # We update the bias
-                self.weights[0] += update 
-                
+                self.weights[0] += update
+
                 # if the update is not zero, then we have an error.
-                errors += int(update != 0.0)            
+                errors += int(update != 0.0)
                 self.errors_.append(errors)
-            
-            #plot_data(self, X,y, self.weights[1:])   
+
+            #plot_data(self, X,y, self.weights[1:])
             return self
-        
-        
+
+
     # activition function
     def net_input(self, X):
         # \vec x \dot \vec w + bias
-        
+
         z = np.dot(X, self.weights[1:]) + self.weights[0] #bias is the first entry in the weight vector
         z = ActivFunc(z)
         # print(z)
         return z
-    
-    
+
+
     #https://numpy.org/doc/stable/reference/generated/numpy.where.html
     def predict(self, X):
         # if get back a vector of 1,0 where 1 is for positive function from the activision function
         return np.where(self.net_input(X) >= 0, 1, 0)# possible last entry is -1
-    
+
 
     def compute(self):
         X,y = load_data_set(False)
@@ -109,30 +139,43 @@ class Perceptron(object):
         plt.ylabel('Number of updates')
         plt.show()
 
-    
-    
-    
-    
-    
+
+
+
     #aux function, not working yet
-    
     #https://stats.stackexchange.com/questions/71335/decision-boundary-plot-for-a-perceptron
-    def plot_data(self,inputs,targets,weights):
+    def plot_data(self):#,inputs,targets,weights):
+        inputs,targets = load_data_set(False)
+        #print(inputs)
+        #print()
+        #print(inputs[:50])
+
+        #print(targets)
+        #print()
+        #print(targets[:50])
+
+
+        inputs = inputs[:50]
+        targets = targets[:50]
+
+        weights = self.weights[1:]
+
         # fig config
-        plt.figure(figsize=(10,6))
+        #plt.figure(figsize=(10,6))
+
         plt.grid(True)
 
-        #plot input samples(2D data points) and i have two classes. 
+        #plot input samples(2D data points) and i have two classes.
         #one is +1 and second one is -1, so it red color for +1 and blue color for -1
         for input,target in zip(inputs,targets):
             plt.plot(input[0],input[1],'ro' if (target == 1.0) else 'bo')
 
         # Here i am calculating slope and intercept with given three weights
         for i in np.linspace(np.amin(inputs[:,:1]),np.amax(inputs[:,:1])):
-            slope = -(weights[0]/weights[2])/(weights[0]/weights[1])  
+            slope = -(weights[0]/weights[2])/(weights[0]/weights[1])
             intercept = -weights[0]/weights[2]
 
             #y =mx+c, m is slope and c is intercept
             y = (slope*i) + intercept
             plt.plot(i, y,'ko')
-
+        plt.show()
