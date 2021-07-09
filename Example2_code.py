d9af5e9ceee1ce3e51054e257bb115f70ebd8da8
Natanael <alpay100@mail.chapman.edu>
2021-07-08 20:05:41 -0700

    add

--- Example2_code.py
+++ Example2_code.py
@@ -26,7 +26,7 @@ def load_data_set(show = True, line = False, weights = []):
     """ Show- true graph, else no grapth"""
     #https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
 
-    # return the data and the target
+    # return the data and the d
     X,y = load_iris(return_X_y=True)
 
     if (show == True):
@@ -77,29 +77,31 @@ class Perceptron(object):
     layer in a multilayer Perceptron system
     self - the thing.
     learning_rate - the wieght, eta.
-    n_iter - number of iteration
+    n_epoch - number of iteration
     random_state - to see later.
     """
 
     # constractor
-    def __init__(self, learning_rate=0.01, n_iter=100, random_state=1):
+    def __init__(self, learning_rate=0.01, n_epoch=100, random_state=1):
         self.learning_rate = learning_rate
-        self.n_iter = n_iter
+        self.n_epoch = n_epoch
         self.random_state = random_state
 
-    def fit(self, X, y):
-        """ Note: In the book inpt is Y in code input is X. In Book target is D, in code target is Y.
-        Maps the traings data X and the target Y, then updates and return the perceptron.
+
+    def fit(self, X, D):
+        """ Train the perceptron using LMS.
+        Maps the traings data X and the d, then updates and return the
+        perceptron.
         """
         rand = np.random.RandomState(self.random_state)
         # create a vector of weights of of size 'size of x + 1' with first entery as bias.
         self.weights = rand.normal(loc=0.0, scale=0.01, size=1 +  X.shape[1])
         self.errors_ = []
-        for _ in range(self.n_iter):
+        for _ in range(self.n_epoch):
             errors = 0
-            for x, target in zip(X, y):
+            for x, d in zip(X, D):
                 # local gradient: delta_j
-                update = self.learning_rate * (target - self.predict(x))
+                update = self.learning_rate * (d - self.predict(x))
 
                 # Weight correction. i.e. we update the weights as in the back-propogation algorithm.
                 self.weights[1:] += update * x
@@ -127,12 +129,11 @@ class Perceptron(object):
     # method documentation https://numpy.org/doc/stable/reference/generated/numpy.where.html
     def predict(self, X):
         """ Note: This part is still in understanding stage.
-
         Prediction is made based upon the summation result (?),
         returns an array of 0 1 based on the activation is positive or not.
         """
         # if get back a vector of 1,0 where 1 is for positive function from the activision function
-        return np.where(self.net_input(X) >= 0, 1, 0)# possible last entry is -1
+        return np.where(self.net_input(X) >= 0, 1, -1)# possible last entry is -1
 
     def compute(self):
         # plotting the error-energy function \xi_avg
@@ -175,8 +176,8 @@ class Perceptron(object):
 
         #plot input samples(2D data points) and i have two classes.
         #one is +1 and second one is -1, so it red color for +1 and blue color for -1
-        for input,target in zip(inputs,targets):
-            plt.plot(input[0],input[1],'ro' if (target == 1.0) else 'bo')
+        for input,d in zip(inputs,targets):
+            plt.plot(input[0],input[1],'ro' if (d == 1.0) else 'bo')
 
         # Here i am calculating slope and intercept with given three weights
         for i in np.linspace(np.amin(inputs[:,:1]),np.amax(inputs[:,:1])):
